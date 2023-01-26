398e9e8791baea1abfe52588b4f0c83f5025d4ee
h0x0er <jatink843@protonmail.com>
2023-01-26 23:40:50 +0530

    showing process args

--- eventhandler.go
+++ eventhandler.go
@@ -54,7 +54,8 @@ func (eventHandler *EventHandler) handleFileEvent(event *Event) {
 	}
 
 	if strings.Contains(event.FileName, "resolved.conf") {
-		WriteAnnotation(fmt.Sprintf("%s Overwrite detected for %s by Exe: %s at [%s]", StepSecurityAnnotationPrefix, resolvedConfigPath, event.Exe, event.Timestamp.Format("2006-01-02T15:04:05.999999999Z")))
+
+		WriteAnnotation(fmt.Sprintf("%s Overwrite detected for %s by Exe: %s at [%s] with args %v", StepSecurityAnnotationPrefix, resolvedConfigPath, event.Exe, event.Timestamp.Format("2006-01-02T15:04:05.999999999Z"), event.ProcessArguments))
 	}
 
 	// Uncomment to log file writes (only uncomment in INT env)
